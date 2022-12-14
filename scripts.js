41449faa0c627699444c7bb6b78e2b3afa7a4b7e
Leinthwirg <niel@wrightclan.com>
2022-12-14 05:01:03 +0000

    Added winCheck, checks for vertical, horizontal and both diagonal 4 in a row

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
-
+const rows = 6
+const columns = 7
 var currentPlayer=1;
 
 function initNewGame(){
@@ -6,20 +7,20 @@ function initNewGame(){
 }
 
 function setCell(col,row,player){
-    let el = document.querySelector(`.gamerow:nth-child(${row})>.cell:nth-child(${col})`);
+    let el = document.querySelector(`.gamerow:nth-child(${row})>.cell:nth-child(${col})`); // CSS Selector https://www.w3schools.com/cssref/sel_nth-child.php#:~:text=Definition%20and%20Usage,name)%2C%20of%20its%20parent.
     el.classList.add(player);
     let plyr = 1; if (player=="player2")plyr=2;
 }
 
 function clearCell(col,row){
-    let el = document.querySelector(`.gamerow:nth-child(${row})>.cell:nth-child(${col})`);  // again with template literals
+    let el = document.querySelector(`.gamerow:nth-child(${row})>.cell:nth-child(${col})`);   
     if (el.classList.contains("player1")) el.classList.remove("player1");
     if (el.classList.contains("player2")) el.classList.remove("player2");
 }
 
 
 function getCell(col,row){
-    let el = document.querySelector(`.gamerow:nth-child(${row})>.cell:nth-child(${col})`);
+    let el = document.querySelector(`.gamerow:nth-child(${row})>.cell:nth-child(${col})`); // Gets the coordinates of x,y then returns either player 1 or 2.
     if (el.classList.contains("player1")) return "player1";
     if (el.classList.contains("player2")) return "player2";
     return "";
@@ -37,18 +38,70 @@ function dropCol(x){
   let y = findBottom(x);
   if (y>0){
     setCell(x,findBottom(x),`player${currentPlayer}`);
-    let newPlayer = (currentPlayer==2)?1:2;   // ternary logic statement  (cond)?iftrue:iffalse if cp=2 then return 1 otherwise return 2 and so on.
-    setPlayer(newPlayer); 
+    const winner=checkWinner();
+    if (winner !==""){
+      alert(winner +"won!");
+      
+    }else{
+      let newPlayer = (currentPlayer==2)?1:2;   // ternary logic statement  Flip flops between first and second player
+      setPlayer(newPlayer);       
+    }
   }
 }
 
-function findBottom(x){
+function findBottom(x){ // add to Y to go down the column, if it runs in to something in cell, go back one.
   for (y=1;y<=6;y++){
     if (getCell(x,y)!="") return y-1; 
   }
   return 6;
 }
 
+function checkWinner() {
+  //horizontal
+  for (let r = 1; r <= rows; r++) {
+    for (let c = 1; c <= columns - 3; c++){
+      if (getCell(c, r) != '') {
+        if (getCell(c, r) == getCell(c+1, r) && getCell(c+1, r) == getCell(c+2, r) && getCell(c+2, r) == getCell(c+3, r)) {
+          return getCell(c,r); 
+        }
+      }
+    }
+  }
+   //vertical (columns)
+    for (let c = 1; c <= columns; c++) {
+    for (let r = 1; r <= rows - 3; r++){
+      if (getCell(c, r) != '') {
+        if (getCell(c, r) == getCell(c, r+1) && getCell(c, r+1) == getCell(c, r+2) && getCell(c, r+2) == getCell(c, r+3)) {
+          //setWinner(c, r);
+          return getCell(c,r); 
+        }
+      }
+    }
+  }
+  
+    //diagonal 1
+  for (let r = 1; r <= rows - 3; r++) {
+    for (let c = 1; c <= columns - 3; c++){
+      if (getCell(c, r) != '') {
+        if (getCell(c, r) == getCell(c+1, r+1) && getCell(c+1, r+1) == getCell(c+2, r+2) && getCell(c+2, r+2) == getCell(c+3, r+3)) {
+          return getCell(c,r); 
+        }
+      }
+    }
+  }
+  
+      //diagonal 2
+  for (let r = 4; r <= rows; r++) {
+    for (let c = 1; c <= columns - 3; c++){
+      if (getCell(c, r) != '') {
+        if (getCell(c, r) == getCell(c+1, r-1) && getCell(c+1, r-1) == getCell(c+2, r-2) && getCell(c+2, r-2) == getCell(c+3, r-3)) {
+          return getCell(c,r); 
+        }
+      }
+    }
+  }
+  return "";
+}
 
 document.addEventListener("DOMContentLoaded", function(event) {
   initNewGame();
